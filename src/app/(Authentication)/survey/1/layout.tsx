8292baf26d06d8f31fd279a768ac93201e62d36f
Chunming <chunming200@gmail.com>
2025-01-12 13:49:30 +0700

    completed authentication structure

--- src/app/(Authentication)/survey/1/layout.tsx
+++ src/app/(Authentication)/survey/1/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../../globals.css";
-import { Providers } from "../../Providers";
+import { Geist, Geist_Mono } from "next/font/google"
+import "../../../globals.css"
+import { Providers } from "../../../Providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   icons: "frint_logo.svg",
-  title: "FRINT - Personalise",
+  title: "FRINT - Personalise#1",
   description: "Your confidence is our confidence.",
 };
 
