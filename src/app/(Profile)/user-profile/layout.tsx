507a9de1b3167c1c8cda7426d798bbd6d6ecbbb7
Chunming <chunming200@gmail.com>
2025-01-31 09:36:58 +0700

    table user-profile updated

--- src/app/(Profile)/user-profile/layout.tsx
+++ src/app/(Profile)/user-profile/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "../../globals.css";
 import { Providers } from "../../Providers";
 import { Nav } from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
